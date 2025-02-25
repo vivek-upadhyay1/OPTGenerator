2046f25008a06e372473b866a0745d81d92361be
Vivek Kumar Upadhyay <77227000+vivek-upadhyay1@users.noreply.github.com>
2025-02-25 09:55:50 +0530

    Update OTPGeneration.java

--- OTPGeneration.java
+++ OTPGeneration.java
@@ -4,6 +4,7 @@ public class OTPGeneration{
 
 public static void main(String[] args){	
 String allNumbers = "0123456789";
+int indexSize = allNumbers.length()-1;
 Scanner sc = new Scanner(System.in);
 System.out.println("Enter the number of digit for OTP");
 int OTPDigits = sc.nextInt();
@@ -11,7 +12,7 @@ StringBuilder sb = new StringBuilder();
 // write a logic to randomly pick the number without any patter and form 6 digit code
 
 for(int i = 0; i<OTPDigits;i++){
-int randomIndex = (int)(Math.random()*9);
+int randomIndex = (int)(Math.random()*indexSize);
 char value = allNumbers.charAt(randomIndex);
 sb.append(value);
 }
